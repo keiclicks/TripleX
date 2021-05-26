d59d04f1c9b7610d13955511232a2c3d1cd86620
Kei <akshaystech@gmail.com>
2021-05-26 18:39:33 +0530

    Reference instead of pointers

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@
 #include <string>
 
 //Sets the default difficulty
-void SetDefaultDifficulty (int *DifficultyPointer) {
-    *DifficultyPointer = 1;
+void SetDefaultDifficulty (int &DifficultyReference) {
+    DifficultyReference = 1;
 }
 
 void PressAnyKeyToContinue() {
@@ -39,39 +39,39 @@ void EntryPrompt() {
 }
 
 //Main game code goes here
-void LoopGameAtDifficulty(int *DifficultyPointer, int *AttemptPointer) {
-    const int CodeA = rand() % (*DifficultyPointer) + (*DifficultyPointer);
-    const int CodeB = rand() % (*DifficultyPointer) + (*DifficultyPointer);
-    const int CodeC = rand() % (*DifficultyPointer) + (*DifficultyPointer);
+void LoopGameAtDifficulty(int &DifficultyReference, int &AttemptReference) {
+    const int CodeA = rand() % DifficultyReference + DifficultyReference;
+    const int CodeB = rand() % DifficultyReference + DifficultyReference;
+    const int CodeC = rand() % DifficultyReference + DifficultyReference;
 //    std::cout << CodeA << " " << CodeB << " " <<CodeC;
-    
+
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
-    
+
     std::cout << "\n>> There are 3 numbers in the code.\n";
     std::cout << ">> The codes add-up to: " << CodeSum << std::endl;
     std::cout << ">> The codes multiply to give: " << CodeProduct << std::endl;
     std::cout << "\nEnter your codes: ";
-    
+
     //Player guess input
     int GuessA, GuessB, GuessC {};
     std::cin >> GuessA >> GuessB >> GuessC;
     int GuessSum {GuessA + GuessB + GuessC};
     int GuessProduct {GuessA * GuessB * GuessC};
-    
+
     //Answer validation
     if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
-        (*DifficultyPointer)++;
+        DifficultyReference++;
         std::cout << "\nYour Codes are correct!!\n";
-        if ((*DifficultyPointer) < 5) {
-            std::cout << "\nIncreasing difficulty level to.. " << (*DifficultyPointer) << std::endl;
+        if (DifficultyReference < 5) {
+            std::cout << "\nIncreasing difficulty level to.. " << DifficultyReference << std::endl;
             PressAnyKeyToContinue();
         }
     }
-    else if ((*AttemptPointer) > 1) {
-        (*AttemptPointer)--;
+    else if (AttemptReference > 1) {
+        AttemptReference--;
         std::cout <<"\nOops! Your codes are incorrect!\n";
-        std::cout <<"\nAttempt remaining : " << (*AttemptPointer) << std::endl;
+        std::cout <<"\nAttempt remaining : " << AttemptReference << std::endl;
         PressAnyKeyToContinue();
     }
     else {
@@ -79,27 +79,27 @@ void LoopGameAtDifficulty(int *DifficultyPointer, int *AttemptPointer) {
         std::cout << "Goodbye.\n";
         exit(0);
     }
-        
+
 }
 
 
 int main() {
-    
+
     srand(time(NULL)); // Create new random sequence based on time of day
-    
+
     int DifficultyLevel {1};
-    SetDefaultDifficulty(&DifficultyLevel);
+    SetDefaultDifficulty(DifficultyLevel);
     const int MaxDifficulty {5};
     int TotalAttempts {3};
     EntryPrompt();
     std::cout << "\nPlaying Game at difficulty "<< DifficultyLevel << std::endl;
     PressAnyKeyToContinue();
     while (DifficultyLevel <= MaxDifficulty && TotalAttempts >= 1) {
-        LoopGameAtDifficulty(&DifficultyLevel,&TotalAttempts);
+        LoopGameAtDifficulty(DifficultyLevel,TotalAttempts);
         std::cin.clear();  //clears the failbit
         std::cin.ignore(); //discarding the buffer
     }
-    
+
     std::cout << "\n\nCongratulations! You open the box and find....\n";
     PressAnyKeyToContinue();
     std::cout << "*** NOTHING!!! ***\n\n";
